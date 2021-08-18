8d8da0b05f2158ec96d6cdf3f5cd1f7ff2e5853b
maxd66 <max.dunn63@gmail.com>
2021-08-18 00:12:30 -0700

    added to contact form and resume form, no functionality yet

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,5 +1,22 @@
 function Contact() {
-  return;
+  return (
+    <form
+      action="mailto:max.dunn63@gmail.com"
+      method="POST"
+      encType="multipart/form-data"
+      name="EmailForm"
+    >
+      <label for="userName">Name:</label>
+      <input type="text" name="userName" id="userName" />
+      <label for="userEmail">Email:</label>
+      <input type="email" name="userEmail" id="userEmail" />
+      <label for="userMessage">Message:</label>
+      <textarea name="userMessage" id="userMessage" rows="6"></textarea>
+      <button type="submit" value="Submit">
+        Send
+      </button>
+    </form>
+  );
 }
 
 export default Contact;
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,5 +1,30 @@
 function Resume() {
-  return;
+  return (
+    <div>
+      <h2>
+        <a href="#resume">Download My Full Resume</a>
+      </h2>
+      <h3>Front End Proficiencies:</h3>
+      <ul>
+        <li>HTML/CSS</li>
+        <li>React</li>
+        <li>jQuery</li>
+        <li>Responsive Design</li>
+        <li>AJAX</li>
+        <li>Bootstrap</li>
+        <li>Bulma</li>
+      </ul>
+      <h3>Back End Proficiencies:</h3>
+      <ul>
+        <li>Node</li>
+        <li>MySql/Sequelize</li>
+        <li>MongoDB/Mongoose</li>
+        <li>Express</li>
+        <li>Graphql/Apollo</li>
+        <li>Efficient Data Storage and Retrieval</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Resume;
